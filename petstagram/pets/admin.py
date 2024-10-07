a5f6920a268b873a8d8180022d815f9a5a1fc649
vegaFlex <vega_flex@abv.bg>
2024-10-07 13:01:23 +0300

    test asd

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram.pets import models
+
+
+@admin.register(models.Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -5,7 +5,7 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(null=True, blank=True)
-    slug = models.SlugField(null = False, blank=True, unique=True)
+    slug = models.SlugField(null = False, blank=True, unique=True, editable=False)
 
     def save(self, *args, **kwargs):
         if not self.slug:
