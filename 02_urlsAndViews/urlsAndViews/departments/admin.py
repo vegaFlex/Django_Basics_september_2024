2453529dc4f87f5d3b1dd02679c37d969c414293
vegaFlex <vega_flex@abv.bg>
2024-10-09 15:54:06 +0300

    fix some elements

--- 02_urlsAndViews/urlsAndViews/departments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from urlsAndViews.departments.models import Department
-
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    pass
--- PetsTagram_APP/petstagram/pets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from petstagram.pets import models
-
-
-@admin.register(models.Pet)
-class PetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
